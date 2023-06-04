5ba627e44d3a8cb961937d4c0d6df23146cc260f
ngo quang trung <quangtrungxf@gmail.com>
2023-06-04 03:18:54 +0200

    changer marker du wagon logo à prix par nuit

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,7 +9,7 @@ class FlatsController < ApplicationController
         lat: flat.latitude,
         lng: flat.longitude,
         info_window: render_to_string(partial: "info_window", locals: {flat: flat}),
-        marker_html: render_to_string(partial: "marker")
+        marker_html: render_to_string(partial: "marker", locals: {flat: flat})
       }
     end
   end
