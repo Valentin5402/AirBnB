5bc090a779367e2673d467dfe259a8c4a8a10a22
Valentin MEROU <valentin.merou@gmail.com>
2023-06-10 00:29:29 +0200

    problème des reviews en grande partie réglé

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,14 +12,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @flat = Flat.find(params[:id])
     @review = Review.new(review_params)
     @review.user = current_user
     @review.booking_id = Booking.where(flat_id: params['flat_id'], user_id: current_user.id)[0].id
     authorize @review
     if @review.save
       redirect_to flat_path(params['flat_id'])
-    else # ATTENTION A CORRIGER
-      # render 'flats/show', status: :unprocessable_entity
+    else # ATTENTION A CORRIGER, il n'y a pas les erreurs de validation
+      render 'flats/show(@flat)', status: :unprocessable_entity
       # @bookings = policy_scope(Booking.where(user_id: current_user.id))
       # render 'bookings/index', status: :unprocessable_entity
     end
