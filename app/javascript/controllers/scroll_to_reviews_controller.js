884abd631a3fc0b86553dc1f4e90b51f0c3d2247
Valentin MEROU <valentin.merou@gmail.com>
2023-06-12 05:11:31 +0200

    essai avec succès

--- app/javascript/controllers/scroll_to_reviews_controller.js
+++ app/javascript/controllers/scroll_to_reviews_controller.js
@@ -2,17 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="scroll-to-reviews"
 export default class extends Controller {
-  static targets = ["links", "reviews"];
+  static targets = ["reviews"];
 
   connect() {
-    console.log("Hello from scroll-to-reviews-controller");
-
+    /* console.log("Hello from scroll-to-reviews-controller"); */
+    /* this.scroll(); */
   }
 
   scroll() {
-    console.log(this.event);
-    // qu'est-ce que je dois mettre en entrée pour scroller ???
-    console.log("I want to scroll !");
-    console.log(this.reviewsTarget);
+    /* console.log(this.element); */
+    /* console.log("I want to scroll !");
+    console.log(this.linksTarget); */
+    /* console.log(this.reviewsTarget); */
+    this.reviewsTarget.scrollIntoView();
   }
 }
